55faee049d0c683f928f02c907caeaf946b9e39e
Gust <ruchira.swwarnapriya8@gmail.com>
2024-08-20 08:40:47 +0530

    Add push notifications

--- App.tsx
+++ App.tsx
@@ -1,21 +1,58 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
-import './gesture-handler';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
-
+import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
+import FlashMessage from 'react-native-flash-message';
+
+import './gesture-handler';
+
 import {persistor, store} from './src/redux/store';
 
 import MainRoute from './src/navigation/routes';
-import {Provider} from 'react-redux';
+import usePushNotification from './src/hooks/usePushNotification';
 
 function App(): React.JSX.Element {
+  const {
+    requestUserPermission,
+    getFCMToken,
+    listenToForegroundNotifications,
+    listenToBackgroundNotifications,
+    onNotificationOpenedAppFromBackground,
+    onNotificationOpenedAppFromQuit,
+  } = usePushNotification();
+
+  useEffect(() => {
+    const listenToNotifications = () => {
+      try {
+        getFCMToken();
+        requestUserPermission();
+        onNotificationOpenedAppFromQuit();
+        listenToForegroundNotifications();
+        listenToBackgroundNotifications();
+        onNotificationOpenedAppFromBackground();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    listenToNotifications();
+  }, [
+    getFCMToken,
+    listenToBackgroundNotifications,
+    listenToForegroundNotifications,
+    onNotificationOpenedAppFromBackground,
+    onNotificationOpenedAppFromQuit,
+    requestUserPermission,
+  ]);
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <GestureHandlerRootView style={styles.mainContainer}>
           <SafeAreaView style={styles.mainContainer}>
             <MainRoute />
+            <FlashMessage position="top" />
           </SafeAreaView>
         </GestureHandlerRootView>
       </PersistGate>
